1a0cf25f60e3828cebe729b3b8c12eab6fbd53e6
Cory Logan <he@corylogan.com>
2016-03-19 14:37:35 -0700

    remove old plugin sets after updates

--- update_plugins.sh
+++ update_plugins.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.vim/plug_snapshots
+snapshot_dir="$HOME/.vim/plug_snapshots"
+mkdir -p "${snapshot_dir}"
 timestamp=`date +%Y-%m-%d:%H:%M:%S`
 
 # Upgrade plug.vim
@@ -9,3 +10,10 @@ timestamp=`date +%Y-%m-%d:%H:%M:%S`
 # Quit vim
 
 vim +PlugUpgrade +PlugUpdate +"PlugSnapshot! ~/.vim/plug_snapshots/${timestamp}_plug_snapshot.vim" +qall;
+
+# Remove old snapshots if there are more than five
+# the `command` keyword allows us to run `ls` without any of the flags
+# that might be aliased into the default.
+
+# cd "${snapshot_dir}"; command ls -tr | head -n 9 | xargs rm
+cd "${snapshot_dir}"; (command ls -t | head -n 5; command ls) | sort | uniq -u | xargs rm
